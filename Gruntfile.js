182784ba740306b29b6d959d37ec308cbd5be0a0
Adnene KHALFA <adnene.khalfa@gmail.com>
2015-07-19 15:35:03 +0200

    Adding Gruntfile + grunt-bump + grunt-changelog

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: 'rc',
+                regExp: false
+            }
+        },
+        changelog: {
+            sample: {
+                options: {
+                    // Task-specific options go here.
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-changelog');
+};
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 [![Stories in Ready](https://badge.waffle.io/c10h22/graphql-vogels.png?label=ready&title=Ready)](https://waffle.io/c10h22/graphql-vogels)
 [![Build Status](https://travis-ci.org/c10h22/graphql-vogels.svg?branch=master)](https://travis-ci.org/c10h22/graphql-vogels)
 [![Coverage Status](https://coveralls.io/repos/c10h22/graphql-vogels/badge.svg?branch=master&service=github)](https://coveralls.io/github/c10h22/graphql-vogels?branch=master)
+[![Dependency Status](https://www.versioneye.com/user/projects/55aba34b30653500230003f9/badge.svg?style=flat)](https://www.versioneye.com/user/projects/55aba34b30653500230003f9)
+# Graphql-Vogels
 
-First commit
+> Vogels helper for graphql
+
+## Table of Contents
