d0bba45bc270da7905c7f7117489a8832330d426
Adnene KHALFA <adnene.khalfa@gmail.com>
2015-07-21 03:37:05 +0200

    fixes #3: Chagelog.txt must be formated in md format and extension

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,12 @@
 
 module.exports = function (grunt) {
     grunt.initConfig({
+        pkg: {
+            version: '0.1.0'
+        },
         bump: {
             options: {
-                files: ['package.json'],
+                files: ['package.json', 'Gruntfile.js'],
                 updateConfigs: [],
                 commit: true,
                 commitMessage: 'Release v%VERSION%',
@@ -23,7 +26,23 @@ module.exports = function (grunt) {
         changelog: {
             initial: {
                 options: {
-                    after: '0.0.1'
+                    after: 'v0.0.2',
+                    n: ',,,',
+                    featureRegex: /^(.*)closes #\d+:?(.*)$/gim,
+                    fixRegex: /^(.*)fixes #\d+:?(.*)$/gim,
+                    dest: 'changelog_<%= pkg.version %>.md',
+                    template: '## Version <%= pkg.version %>({{date}})\n\n{{> features}}{{> fixes}}',
+                    logArguments: [
+                        '--pretty=- (%h) - %ad: %s',
+                        '--no-merges',
+                        '--date=short'
+                    ],
+                    partials: {
+                        features: '###FEATURES:###\n\n{{#if features}}{{#each features}}{{> feature}}{{/each}}{{else}}{{> empty}}{{/if}}\n',
+                        feature: '{{this}} {{this.date}}\n',
+                        fixes: '###FIXES:###\n\n{{#if fixes}}{{#each fixes}}{{> fix}}{{/each}}{{else}}{{> empty}}{{/if}}\n',
+                        fix: '{{this}} {{this.date}}\n'
+                    }
                 }
             }
         }
---
+++ changelog_0.1.0.md
@@ -0,0 +1,10 @@
+## Version 0.1.0 from  (2015-07-21)
+
+###FEATURES:###
+
+  (none)
+
+###FIXES:###
+
+- (7147b3b) - 2015-07-21:  changelog not updated 
+
